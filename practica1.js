041b00d64f7d9d2989887d2d0a2811916d4f0f1d
Mariana Alarcón <124741193+alarconcita@users.noreply.github.com>
2023-03-08 10:42:24 -0300

    Add files via upload

---
+++ practica1.js
@@ -0,0 +1,106 @@
+/*
+console.log("EJERCICIO_1");
+let name = "Ross";
+let surname = "Geller";
+let age = 33;
+
+console.log("EJERCICIO_2");
+parseInt(age);
+console.log("Nombre", name);
+console.log("tipo: ", typeof 'name');
+console.log("Apellido: ", surname);
+console.log("tipo: ", typeof 'surname');
+console.log("Edad: ", age);
+console.log("tipo: ", typeof 'age');
+
+console.log("EJERCICIO_3");
+let city;
+city = "CentralPerk";
+console.log(city);
+city = "MonicasDpto";
+console.log(city);
+city = "JoeysDpto";
+console.log(city);
+city = "RosssDpto";
+console.log(city);
+city = "TrabajoDeChandler";
+console.log(city);
+
+console.log("EJERCICIO_4");
+// Predicción: string
+console.log(typeof(20)); //le erré, es number
+
+//Predicción: number
+console.log(typeof(95.5)); //bien
+
+//Predicción: NaN
+console.log(typeof(NaN)); //le erré, es number
+
+//Predicción: string
+console.log(typeof("hello")); //bien
+
+//Predicción: boolean
+console.log(typeof(true)); //bien
+
+//Predicción: boolean
+console.log(typeof(1 != 2)); //bien
+
+//Predicción: number 
+console.log(typeof("1" - "3")); //bien
+
+//Predicción: string
+console.log(typeof("hamburguer"+"s")); //bien
+
+//Predicción: string
+console.log(typeof("1" + "3")); //bien
+
+//Predicción: string
+console.log(typeof("johnny" + "5")); //bien
+
+//Predicción: string
+console.log(typeof("johnny" - "5")); //le erré, es number
+
+//Predicción: string
+console.log(typeof(99*"luftbaloons")); //le erré, es number
+
+//Predicción: string
+console.log(typeof("hamburguer"+"s")); //bien
+
+
+console.log("EJERCICIO_5");
+
+console.log('al que madruga Dios lo ayuda'.toUpperCase());
+
+console.log("EJERCICIO_6");
+
+console.log('MÁS VALE PÁJARO EN MANO QUE CIENTOS VOLANDO'.toLowerCase());
+
+
+console.log("EJERCICIO_7");
+
+const saludo = 'HoLa'
+const saludo2 = 'mUnDo'
+console.log("'"+saludo.toLowerCase() + ' ' + saludo2.toUpperCase()+"'");
+
+
+console.log("EJERCICIO_8");
+let precio = 100
+let descuento = 20
+let preciod = precio - (descuento*precio/100);
+console.log("Su producto tiene un precio de " + (preciod) + ", " + "el descuento fue de " + descuento + "%");
+
+*/
+console.log("EJERCICIO_9");
+let num1 = 56;
+let num2 = 89;
+let num3 = 7;
+let num4 = 356;
+let num5 = 41;
+let prom = (num1+num2+num3+num4+num5)/5;
+console.log("el promedio es " + prom);
+
+
+console.log("EJERCICIO_10");
+let diam = 6
+const per = Math.PI*diam;
+console.log("el perímetro es: " + per);
